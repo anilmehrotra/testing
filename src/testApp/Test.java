b5e257864e02567aad900058dde0bccb6c1f98d2
techm-mehrotraa <techm-mehrotraa@cc-24-e6-w5018.MCD.GOV.IN>
2015-08-05 16:41:43 +0530

    d

--- src/testApp/Test.java
+++ src/testApp/Test.java
@@ -1,23 +1,26 @@
 package testApp;
 
-public class Test extends Thread{
+import java.util.HashMap;
+import java.util.Map;
 
-	public void run(){
-		for(int i=0;i<3;i++)
-		System.out.println(Thread.currentThread().getName());
+public class Test implements Runnable {
+	private static Map<String, String> hmap=new HashMap<String, String>();
+	
+	public void run() {
+		for(int i=0;i<25;i++){
+			System.out.println(Thread.currentThread().getName());
+			hmap.put(Thread.currentThread().getName()+i, "ALL");
+		}
 	}
 	public static void main(String[] args) throws InterruptedException {
-		Test t0=new Test();
-		Test t1=new Test();
-		Test t2=new Test();
-		t0.start();
-		t0.join();
-		
+		Test t=new Test();
+		Thread t1=new Thread(t);
+		Thread t2=new Thread(t);
+		Thread t3=new Thread(t);
 		t1.start();
-		//t1.join();
 		t2.start();
-		
-	//	t3.join();
-	
+		t3.start();
+		t3.join();
+		System.out.println(hmap);
 	}
 }
